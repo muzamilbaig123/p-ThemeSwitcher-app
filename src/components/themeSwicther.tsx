da1432fc626b764a5023ef6a42640b3018fe3078
= <amberoortiz@gmail.com>
2024-11-06 09:03:25 -0800

    Basic ThemeSwitcher is Completed

--- src/components/themeSwicther.tsx
+++ src/components/themeSwicther.tsx
@@ -1,7 +1,30 @@
-export default function Themes () {
+"use client"
+import { useAppDispatch, useAppSelector } from "@/lib/hooks"
+import { switcher } from "@/lib/slices/themeSwicther";
+
+export default function Themes() {
+
+    const toogleColor = useAppSelector((state) => state.theme.isDarkMode);
+    const dispatch = useAppDispatch();
+
+    const themeHandler = () => {
+        dispatch(switcher())
+    }
+
+    const bgTextDark = {
+        backgroundColor: "#000",
+        color: "#fff" 
+    }
+
     return (
         <>
-            <h1>Themes </h1>
+            <div className="w-full h-screen flex flex-col justify-center items-center gap-3" style={toogleColor ? bgTextDark : {}}>
+
+            <h1 className="text-3xl underline">Themes Switcher App</h1>
+                <input type="checkbox" className="toggle toggle-success" defaultChecked 
+                onClick={themeHandler}
+                />
+            </div>
         </>
     )
 }
